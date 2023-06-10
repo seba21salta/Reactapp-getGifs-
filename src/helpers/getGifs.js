88422970280437e39c5258d0667d1e65499e4dd0
unknown <seba21salta@gmail.com>
2023-06-10 11:47:41 -0300

    datax2

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -6,9 +6,9 @@ export const getGifs = async (category) =>{
   
     const resp = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=F8SkZExXtnY7Gotpvv6VsoRFVkjLaCTj&q=${category}&limit=10`);
     const resp2 = resp.data;
-  
+     const resp3 = resp2.data
    
-    const gifs = resp2.map(img => ({
+    const gifs = resp3.map(img => ({
        id: img?.id,
        title: img?.title,
        url: img?.images?.downsized_medium?.url
